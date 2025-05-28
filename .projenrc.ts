82323599ef793cd09ec408a8b4895089fc3d9e65
Boris Erdmann <boris.erdmann@superluminar.io>
2025-05-28 13:59:26 +0200

    configure projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,31 @@
-import { typescript } from 'projen';
+import { typescript, github } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'transit-gw',
+  name: '@superluminar-io/transit-gw',
+  packageName: 'superluminar-transit-gw-blueprint',
+  description: 'A lean, opinionated blueprint to deploy Transit Gateway in an AWS Organization.',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  autoDetectBin: true,
+  license: 'MIT',
+  authorOrganization: true,
+  authorUrl: 'https://superluminar.io',
+  authorName: 'superluminar GmbH',
+  releaseToNpm: true,
+  release: true,
+  package: true,
+  npmAccess: NpmAccess.PUBLIC,
+  repository: 'https://github.com/superluminar-io/transit-gw.git',
+  deps: [
+    'clipanion',
+    'typescript',
+  ],
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  sampleCode: false,
+  gitignore: ['/blueprints/**/package-lock.json', '/blueprints/**/yarn.lock'],
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp(),
+  },
 });
 project.synth();
